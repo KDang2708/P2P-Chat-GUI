81344dd702b5371e8803dab9c4de682ab0f39747
vanloc05 <loc0944193914@gmail.com>
2026-04-11 22:08:54 +0700

    FE

--- Code/P2PChatGUI/Form1.Designer.cs
+++ Code/P2PChatGUI/Form1.Designer.cs
@@ -2,38 +2,102 @@
 {
     partial class Form1
     {
-        /// <summary>
-        ///  Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        ///  Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.TextBox txtPort;
+        private System.Windows.Forms.RichTextBox txtChat;
+        private System.Windows.Forms.TextBox txtMessage;
+
+        private System.Windows.Forms.Button btnHost;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.Button btnDisconnect;
+
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
-            {
                 components.Dispose();
-            }
+
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        ///  Required method for Designer support - do not modify
-        ///  the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
-        }
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.txtPort = new System.Windows.Forms.TextBox();
+            this.txtChat = new System.Windows.Forms.RichTextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
 
-        #endregion
+            this.btnHost = new System.Windows.Forms.Button();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.btnDisconnect = new System.Windows.Forms.Button();
+
+            this.SuspendLayout();
+
+            // IP
+            txtIP.Location = new System.Drawing.Point(20, 15);
+            txtIP.Size = new System.Drawing.Size(120, 23);
+            txtIP.Name = "txtIP";
+
+            // PORT
+            txtPort.Location = new System.Drawing.Point(150, 15);
+            txtPort.Size = new System.Drawing.Size(70, 23);
+            txtPort.Name = "txtPort";
+
+            // HOST
+            btnHost.Location = new System.Drawing.Point(230, 13);
+            btnHost.Size = new System.Drawing.Size(70, 25);
+            btnHost.Text = "Host";
+            btnHost.Click += new System.EventHandler(this.btnHost_Click);
+
+            // CONNECT
+            btnConnect.Location = new System.Drawing.Point(310, 13);
+            btnConnect.Size = new System.Drawing.Size(80, 25);
+            btnConnect.Text = "Connect";
+            btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+
+            // DISCONNECT
+            btnDisconnect.Location = new System.Drawing.Point(400, 13);
+            btnDisconnect.Size = new System.Drawing.Size(90, 25);
+            btnDisconnect.Text = "Disconnect";
+            btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
+
+            // CHAT BOX
+            txtChat.Location = new System.Drawing.Point(20, 50);
+            txtChat.Size = new System.Drawing.Size(470, 250);
+            txtChat.ReadOnly = true;
+            txtChat.Name = "txtChat";
+
+            // MESSAGE
+            txtMessage.Location = new System.Drawing.Point(20, 310);
+            txtMessage.Size = new System.Drawing.Size(370, 23);
+            txtMessage.Name = "txtMessage";
+            txtMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMessage_KeyDown);
+
+            // SEND
+            btnSend.Location = new System.Drawing.Point(400, 308);
+            btnSend.Size = new System.Drawing.Size(90, 25);
+            btnSend.Text = "Send";
+            btnSend.Click += new System.EventHandler(this.btnSend_Click);
+
+            // FORM
+            this.ClientSize = new System.Drawing.Size(520, 360);
+            this.Controls.Add(txtIP);
+            this.Controls.Add(txtPort);
+            this.Controls.Add(btnHost);
+            this.Controls.Add(btnConnect);
+            this.Controls.Add(btnDisconnect);
+            this.Controls.Add(txtChat);
+            this.Controls.Add(txtMessage);
+            this.Controls.Add(btnSend);
+
+            this.Text = "P2P Chat - Lê Anh Kiệt";
+            this.Load += new System.EventHandler(this.Form1_Load);
+
+            this.ResumeLayout(false);
+            this.PerformLayout();
+        }
     }
 }
--- Code/P2PChatGUI/Form1.cs
+++ Code/P2PChatGUI/Form1.cs
@@ -1,10 +1,113 @@
+using System;
+using System.Drawing;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using P2PChatGUI.Core;
+
 namespace P2PChatGUI
 {
     public partial class Form1 : Form
     {
+        private ChatNetworkCore network;
+
         public Form1()
         {
             InitializeComponent();
+            network = new ChatNetworkCore();
+
+            btnSend.Enabled = false;
+
+            network.OnMessageReceived += (msg) =>
+            {
+                Invoke(new Action(() =>
+                {
+                    txtChat.SelectionColor = Color.Black;
+                    txtChat.AppendText($"[{DateTime.Now:HH:mm}] Đối phương: {msg}\n");
+                    txtChat.ScrollToCaret();
+                }));
+            };
+
+            network.OnSystemMessage += (msg) =>
+            {
+                Invoke(new Action(() =>
+                {
+                    txtChat.SelectionColor = Color.Gray;
+                    txtChat.AppendText($"[System] {msg}\n");
+                }));
+            };
+
+            network.OnDisconnected += () =>
+            {
+                Invoke(new Action(() =>
+                {
+                    txtChat.SelectionColor = Color.Red;
+                    txtChat.AppendText("⚠ Mất kết nối\n");
+                    btnSend.Enabled = false;
+                }));
+            };
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtIP.Text = "127.0.0.1";
+            txtPort.Text = "8888";
+        }
+
+        private async void btnHost_Click(object sender, EventArgs e)
+        {
+            if (await network.StartListeningAsync(txtIP.Text, int.Parse(txtPort.Text)))
+            {
+                txtChat.AppendText("✔ Bạn đang là Host\n");
+                btnSend.Enabled = true;
+                txtMessage.Focus();
+            }
+        }
+
+        private async void btnConnect_Click(object sender, EventArgs e)
+        {
+            if (await network.ConnectAsync(txtIP.Text, int.Parse(txtPort.Text)))
+            {
+                txtChat.AppendText("✔ Đã kết nối\n");
+                btnSend.Enabled = true;
+                txtMessage.Focus();
+            }
+        }
+
+        private async void btnSend_Click(object sender, EventArgs e)
+        {
+            await SendMessage();
+        }
+
+        private async Task SendMessage()
+        {
+            string msg = txtMessage.Text.Trim();
+            if (msg == "") return;
+
+            await network.SendMessageAsync(msg);
+
+            txtChat.SelectionColor = Color.Blue;
+            txtChat.AppendText($"[{DateTime.Now:HH:mm}] Bạn: {msg}\n");
+
+            txtMessage.Clear();
+        }
+
+        private async void txtMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Enter)
+            {
+                await SendMessage();
+                e.SuppressKeyPress = true;
+            }
+        }
+
+        private async void btnDisconnect_Click(object sender, EventArgs e)
+        {
+            await network.DisconnectAsync();
+
+            txtChat.SelectionColor = Color.Red;
+            txtChat.AppendText("❌ Bạn đã ngắt kết nối\n");
+
+            btnSend.Enabled = false;
         }
     }
 }
