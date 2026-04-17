3bfd2b78af8a1e65c0602e3189dd9ff4c79a580c
Gia Huy 1 <giahuy552345@gmail.com>
2026-04-17 18:51:41 +0700

    feat: enhance UI with IP and Port labels, improve button styling, and streamline chat message display

--- Code/P2PChatGUI/Form1.Designer.cs
+++ Code/P2PChatGUI/Form1.Designer.cs
@@ -13,6 +13,8 @@
         private System.Windows.Forms.Button btnConnect;
         private System.Windows.Forms.Button btnSend;
         private System.Windows.Forms.Button btnDisconnect;
+        private System.Windows.Forms.Label lblIP;
+        private System.Windows.Forms.Label lblPort;
 
         protected override void Dispose(bool disposing)
         {
@@ -33,58 +35,118 @@
             this.btnConnect = new System.Windows.Forms.Button();
             this.btnSend = new System.Windows.Forms.Button();
             this.btnDisconnect = new System.Windows.Forms.Button();
+            this.lblIP = new System.Windows.Forms.Label();
+            this.lblPort = new System.Windows.Forms.Label();
 
             this.SuspendLayout();
 
+            // LBL IP
+            lblIP.Text = "IP:";
+            lblIP.Location = new System.Drawing.Point(20, 20);
+            lblIP.AutoSize = true;
+            lblIP.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            lblIP.ForeColor = System.Drawing.Color.FromArgb(100, 100, 100);
+
             // IP
-            txtIP.Location = new System.Drawing.Point(20, 15);
-            txtIP.Size = new System.Drawing.Size(120, 23);
+            txtIP.Location = new System.Drawing.Point(50, 20);
+            txtIP.Size = new System.Drawing.Size(320, 30);
             txtIP.Name = "txtIP";
+            txtIP.Font = new System.Drawing.Font("Segoe UI", 10F);
+            txtIP.BackColor = System.Drawing.Color.FromArgb(245, 245, 245);
+            txtIP.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            txtIP.Multiline = true;
+            txtIP.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtIP_KeyPress);
+
+            // LBL PORT
+            lblPort.Text = "Port:";
+            lblPort.Location = new System.Drawing.Point(390, 20);
+            lblPort.AutoSize = true;
+            lblPort.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold);
+            lblPort.ForeColor = System.Drawing.Color.FromArgb(100, 100, 100);
 
             // PORT
-            txtPort.Location = new System.Drawing.Point(150, 15);
-            txtPort.Size = new System.Drawing.Size(70, 23);
+            txtPort.Location = new System.Drawing.Point(440, 20);
+            txtPort.Size = new System.Drawing.Size(120, 30);
             txtPort.Name = "txtPort";
+            txtPort.Font = new System.Drawing.Font("Segoe UI", 10F);
+            txtPort.BackColor = System.Drawing.Color.FromArgb(245, 245, 245);
+            txtPort.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            txtPort.Multiline = true;
+            txtPort.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPort_KeyPress);
 
             // HOST
-            btnHost.Location = new System.Drawing.Point(230, 13);
-            btnHost.Size = new System.Drawing.Size(70, 25);
+            btnHost.Location = new System.Drawing.Point(20, 60);
+            btnHost.Size = new System.Drawing.Size(180, 35);
             btnHost.Text = "Host";
+            btnHost.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Bold);
+            btnHost.BackColor = System.Drawing.Color.FromArgb(20, 20, 25);
+            btnHost.ForeColor = System.Drawing.Color.White;
+            btnHost.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            btnHost.FlatAppearance.BorderSize = 0;
+            btnHost.Cursor = System.Windows.Forms.Cursors.Hand;
             btnHost.Click += new System.EventHandler(this.btnHost_Click);
 
             // CONNECT
-            btnConnect.Location = new System.Drawing.Point(310, 13);
-            btnConnect.Size = new System.Drawing.Size(80, 25);
+            btnConnect.Location = new System.Drawing.Point(210, 60);
+            btnConnect.Size = new System.Drawing.Size(170, 35);
             btnConnect.Text = "Connect";
+            btnConnect.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Bold);
+            btnConnect.BackColor = System.Drawing.Color.FromArgb(20, 20, 25);
+            btnConnect.ForeColor = System.Drawing.Color.White;
+            btnConnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            btnConnect.FlatAppearance.BorderSize = 0;
+            btnConnect.Cursor = System.Windows.Forms.Cursors.Hand;
             btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
 
             // DISCONNECT
-            btnDisconnect.Location = new System.Drawing.Point(400, 13);
-            btnDisconnect.Size = new System.Drawing.Size(90, 25);
+            btnDisconnect.Location = new System.Drawing.Point(390, 60);
+            btnDisconnect.Size = new System.Drawing.Size(170, 35);
             btnDisconnect.Text = "Disconnect";
+            btnDisconnect.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Bold);
+            btnDisconnect.BackColor = System.Drawing.Color.FromArgb(235, 120, 135);
+            btnDisconnect.ForeColor = System.Drawing.Color.White;
+            btnDisconnect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            btnDisconnect.FlatAppearance.BorderSize = 0;
+            btnDisconnect.Cursor = System.Windows.Forms.Cursors.Hand;
             btnDisconnect.Click += new System.EventHandler(this.btnDisconnect_Click);
 
             // CHAT BOX
-            txtChat.Location = new System.Drawing.Point(20, 50);
-            txtChat.Size = new System.Drawing.Size(470, 250);
+            txtChat.Location = new System.Drawing.Point(20, 110);
+            txtChat.Size = new System.Drawing.Size(540, 320);
             txtChat.ReadOnly = true;
             txtChat.Name = "txtChat";
+            txtChat.Font = new System.Drawing.Font("Segoe UI", 10F);
+            txtChat.BackColor = System.Drawing.Color.White;
+            txtChat.BorderStyle = System.Windows.Forms.BorderStyle.None;
 
             // MESSAGE
-            txtMessage.Location = new System.Drawing.Point(20, 310);
-            txtMessage.Size = new System.Drawing.Size(370, 23);
+            txtMessage.Location = new System.Drawing.Point(20, 445);
+            txtMessage.Size = new System.Drawing.Size(430, 30);
             txtMessage.Name = "txtMessage";
+            txtMessage.Font = new System.Drawing.Font("Segoe UI", 10F);
+            txtMessage.BackColor = System.Drawing.Color.FromArgb(245, 245, 245);
+            txtMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            txtMessage.Multiline = true;
             txtMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMessage_KeyDown);
 
             // SEND
-            btnSend.Location = new System.Drawing.Point(400, 308);
-            btnSend.Size = new System.Drawing.Size(90, 25);
+            btnSend.Location = new System.Drawing.Point(460, 445);
+            btnSend.Size = new System.Drawing.Size(100, 30);
             btnSend.Text = "Send";
+            btnSend.Font = new System.Drawing.Font("Segoe UI", 9.5F, System.Drawing.FontStyle.Bold);
+            btnSend.BackColor = System.Drawing.Color.FromArgb(245, 245, 245);
+            btnSend.ForeColor = System.Drawing.Color.Black;
+            btnSend.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            btnSend.FlatAppearance.BorderSize = 0;
+            btnSend.Cursor = System.Windows.Forms.Cursors.Hand;
             btnSend.Click += new System.EventHandler(this.btnSend_Click);
 
             // FORM
-            this.ClientSize = new System.Drawing.Size(520, 360);
+            this.ClientSize = new System.Drawing.Size(580, 500);
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(lblIP);
             this.Controls.Add(txtIP);
+            this.Controls.Add(lblPort);
             this.Controls.Add(txtPort);
             this.Controls.Add(btnHost);
             this.Controls.Add(btnConnect);
--- Code/P2PChatGUI/Form1.cs
+++ Code/P2PChatGUI/Form1.cs
@@ -15,25 +15,35 @@ namespace P2PChatGUI
             InitializeComponent();
             network = new ChatNetworkCore();
 
+            btnHost.EnabledChanged += Button_EnabledChanged;
+            btnConnect.EnabledChanged += Button_EnabledChanged;
+            btnDisconnect.EnabledChanged += Button_EnabledChanged;
+            btnSend.EnabledChanged += Button_EnabledChanged;
+
             btnSend.Enabled = false;
             btnDisconnect.Enabled = false;
 
             network.OnMessageReceived += (msg) =>
             {
-                Invoke(new Action(() =>
-                {
-                    txtChat.SelectionColor = Color.Black;
-                    txtChat.AppendText($"[{DateTime.Now:HH:mm}] Đối phương: {msg}\n");
-                    txtChat.ScrollToCaret();
-                }));
+                Invoke(new Action(() => AppendChatText("Đối phương", msg, Color.FromArgb(100, 100, 100), HorizontalAlignment.Left)));
             };
 
             network.OnSystemMessage += (msg) =>
             {
                 Invoke(new Action(() =>
                 {
-                    txtChat.SelectionColor = Color.Gray;
-                    txtChat.AppendText($"[System] {msg}\n");
+                    if (msg.StartsWith("Lỗi") || msg.Contains("ngắt") || msg.Contains("thoát"))
+                    {
+                        AppendStyledText(msg, Color.FromArgb(235, 87, 87), Color.FromArgb(253, 237, 237), Color.FromArgb(180, 50, 50));
+                    }
+                    else if (msg.Contains("thành công") || msg.Contains("tới") || msg.Contains("Host"))
+                    {
+                        AppendStyledText(msg, Color.FromArgb(39, 174, 96), Color.FromArgb(232, 248, 245), Color.FromArgb(39, 174, 96));
+                    }
+                    else
+                    {
+                        AppendStyledText(msg, Color.FromArgb(45, 156, 219), Color.FromArgb(234, 242, 248), Color.FromArgb(41, 128, 185));
+                    }
                 }));
             };
 
@@ -41,14 +51,76 @@ namespace P2PChatGUI
             {
                 Invoke(new Action(() =>
                 {
-                    txtChat.SelectionColor = Color.Red;
-                    txtChat.AppendText("⚠ Mất kết nối\n");
+                    AppendStyledText("Mất kết nối", Color.FromArgb(235, 87, 87), Color.FromArgb(253, 237, 237), Color.FromArgb(180, 50, 50));
                     btnSend.Enabled = false;
                     btnHost.Enabled = true;
                     btnConnect.Enabled = true;
                     btnDisconnect.Enabled = false;
                 }));
             };
+        }
+
+        private void Button_EnabledChanged(object sender, EventArgs e)
+        {
+            if (sender is Button btn)
+            {
+                if (btn.Enabled)
+                {
+                    if (btn == btnDisconnect)
+                    {
+                        btn.BackColor = Color.FromArgb(235, 120, 135);
+                        btn.ForeColor = Color.White;
+                    }
+                    else if (btn == btnSend)
+                    {
+                        btn.BackColor = Color.FromArgb(245, 245, 245);
+                        btn.ForeColor = Color.Black;
+                    }
+                    else // Host và Connect
+                    {
+                        btn.BackColor = Color.FromArgb(20, 20, 25);
+                        btn.ForeColor = Color.White;
+                    }
+                }
+                else
+                {
+                    btn.BackColor = Color.FromArgb(230, 230, 230);
+                    btn.ForeColor = Color.Gray;
+                }
+            }
+        }
+
+        private void AppendStyledText(string text, Color barColor, Color bgColor, Color textColor)
+        {
+            txtChat.SelectionStart = txtChat.TextLength;
+            txtChat.SelectionLength = 0;
+            txtChat.SelectionAlignment = HorizontalAlignment.Left;
+            txtChat.SelectionBackColor = bgColor;
+
+            txtChat.SelectionFont = new Font("Segoe UI", 12F, FontStyle.Bold);
+            txtChat.SelectionColor = barColor;
+            txtChat.AppendText(" ▌");
+
+            txtChat.SelectionFont = new Font("Segoe UI", 10F, FontStyle.Regular);
+            txtChat.SelectionColor = textColor;
+            txtChat.AppendText(" " + text.PadRight(100) + "\n\n");
+
+            txtChat.SelectionBackColor = txtChat.BackColor;
+            txtChat.SelectionStart = txtChat.TextLength;
+            txtChat.ScrollToCaret();
+        }
+
+        private void AppendChatText(string prefix, string msg, Color prefixColor, HorizontalAlignment alignment)
+        {
+            txtChat.SelectionStart = txtChat.TextLength;
+            txtChat.SelectionLength = 0;
+            txtChat.SelectionAlignment = alignment;
+            txtChat.SelectionColor = prefixColor;
+            txtChat.AppendText(prefix + ": ");
+            txtChat.SelectionColor = Color.FromArgb(30, 30, 30);
+            txtChat.AppendText(msg + "\n\n");
+            txtChat.SelectionStart = txtChat.TextLength;
+            txtChat.ScrollToCaret();
         }
 
         private void Form1_Load(object sender, EventArgs e)
@@ -65,7 +137,7 @@ namespace P2PChatGUI
 
             if (await network.StartListeningAsync(txtIP.Text, int.Parse(txtPort.Text)))
             {
-                txtChat.AppendText("✔ Bạn đang là Host\n");
+                AppendStyledText("Bạn đang là Host", Color.FromArgb(39, 174, 96), Color.FromArgb(240, 255, 240), Color.FromArgb(39, 174, 96));
                 btnSend.Enabled = true;
                 txtMessage.Focus();
             }
@@ -85,7 +157,7 @@ namespace P2PChatGUI
 
             if (await network.ConnectAsync(txtIP.Text, int.Parse(txtPort.Text)))
             {
-                txtChat.AppendText("✔ Đã kết nối\n");
+                AppendStyledText("Đã kết nối", Color.FromArgb(39, 174, 96), Color.FromArgb(240, 255, 240), Color.FromArgb(39, 174, 96));
                 btnSend.Enabled = true;
                 txtMessage.Focus();
             }
@@ -109,8 +181,7 @@ namespace P2PChatGUI
 
             await network.SendMessageAsync(msg);
 
-            txtChat.SelectionColor = Color.Blue;
-            txtChat.AppendText($"[{DateTime.Now:HH:mm}] Bạn: {msg}\n");
+            AppendChatText("Bạn", msg, Color.FromArgb(100, 100, 100), HorizontalAlignment.Right);
 
             txtMessage.Clear();
         }
@@ -132,13 +203,30 @@ namespace P2PChatGUI
         {
             await network.DisconnectAsync();
 
-            txtChat.SelectionColor = Color.Red;
-            txtChat.AppendText("❌ Bạn đã ngắt kết nối\n");
+            AppendStyledText("Bạn đã ngắt kết nối", Color.FromArgb(235, 87, 87), Color.FromArgb(253, 237, 237), Color.FromArgb(180, 50, 50));
 
             btnSend.Enabled = false;
             btnHost.Enabled = true;
             btnConnect.Enabled = true;
             btnDisconnect.Enabled = false;
+        }
+
+        private void txtPort_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Chỉ cho phép nhập số và các phím điều khiển (như Backspace)
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void txtIP_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // IP thường chỉ có số và dấu phẩy/chấm
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) && e.KeyChar != '.')
+            {
+                e.Handled = true;
+            }
         }
     }
 }
